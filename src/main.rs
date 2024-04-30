6720d8b07f539d553766f817e5cb42050683868f
Aravind I M <aravindim@yahoo.com>
2024-04-30 23:50:37 +0530

    feat(prompt): basic prompt

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use rustyline::error::ReadlineError;
+use rustyline::{DefaultEditor, Result};
+
+fn main() -> Result<()> {
+    let mut rl = DefaultEditor::new()?;
+    println!("Welcome to Lime interpreter!");
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str())?;
+                println!("Output: {}", line);
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("Error: Keyboard Interrupt!");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("Bye!");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+
+    Ok(())
+}
