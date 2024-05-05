14202a8498cc4f970e64390b37f73c4516655abb
Aravind I M <aravindim@yahoo.com>
2024-05-05 12:23:07 +0530

    test(interpreter): lexer output preview after prompt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use lime::lexer::{Lexer, Token};
 use rustyline::error::ReadlineError;
 use rustyline::{DefaultEditor, Result};
 
@@ -9,7 +10,34 @@ fn main() -> Result<()> {
         match readline {
             Ok(line) => {
                 rl.add_history_entry(line.as_str())?;
-                println!("Output: {}", line);
+                let mut lexer = Lexer::new(&line);
+                loop {
+                    match lexer.next() {
+                        Ok(token) => match token {
+                            Token::Start { line, col } => {
+                                println!("Start(line: {}, col: {})", line, col)
+                            }
+                            Token::End { line, col } => {
+                                println!("End(line: {}, col: {})", line, col)
+                            }
+                            Token::String { token, line, col } => {
+                                println!("String(\"{}\", line: {}, col: {})", token, line, col)
+                            }
+                            Token::Number { token, line, col } => {
+                                println!("Number({}, line: {}, col: {})", token, line, col)
+                            }
+                            Token::Symbol { token, line, col } => {
+                                println!("Symbol({}, line: {}, col: {})", token, line, col)
+                            }
+                        },
+                        Err(lime::lexer::LexerError::UnclosedString { .. }) => {
+                            println!("Error:Missing quote");
+                            break;
+                        }
+                        Err(lime::lexer::LexerError::NoTokenFound) => break,
+                    };
+                }
+                // println!("Output: {}", start);
             }
             Err(ReadlineError::Interrupted) => {
                 println!("Error: Keyboard Interrupt!");
