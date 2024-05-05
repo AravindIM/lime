c608ab3fed529e928b788af20e8900d22f891416
Aravind I M <aravindim@yahoo.com>
2024-05-05 11:57:57 +0530

    feat(lexer): added Token enum

---
+++ src/lexer.rs
@@ -0,0 +1,25 @@
+pub enum Token {
+    Start {
+        line: usize,
+        col: usize,
+    },
+    End {
+        line: usize,
+        col: usize,
+    },
+    Symbol {
+        token: String,
+        line: usize,
+        col: usize,
+    },
+    String {
+        token: String,
+        line: usize,
+        col: usize,
+    },
+    Number {
+        token: String,
+        line: usize,
+        col: usize,
+    },
+}
