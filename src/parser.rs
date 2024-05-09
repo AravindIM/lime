be2ea557cf5b4b546408f92ae2af0a497eba74e1
Aravind I M <aravindim@yahoo.com>
2024-05-09 08:56:17 +0530

    feat(parser): added parse function with lexer error handling

--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,5 @@
+use crate::lexer::{Lexer, LexerError};
+
 pub enum AstNode<'a> {
     List {
         elements: Vec<Box<AstNode<'a>>>,
@@ -25,4 +27,20 @@ pub enum ParserError {
     UnclosedString { line: usize, col: usize },
     UnclosedList { line: usize, col: usize },
     NotAList { line: usize, col: usize },
+}
+
+pub fn parse<'a>(lexer: &'a mut Lexer) -> Result<Vec<AstNode<'a>>, ParserError> {
+    let mut program: Vec<AstNode> = vec![];
+    loop {
+        match lexer.next() {
+            Ok(token) => {}
+            Err(error) => match error {
+                LexerError::UnclosedString { line, col } => {
+                    return Err(ParserError::UnclosedList { line, col });
+                }
+                LexerError::NoTokenFound => break,
+            },
+        }
+    }
+    return Ok(program);
 }
